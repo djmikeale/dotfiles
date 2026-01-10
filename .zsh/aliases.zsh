e44df4318573faae490a253c427c2496a034f446
Mikael Thorup <mhthorup@gmail.com>
2026-01-11 00:32:40 +0100

    clean up more again

--- .zsh/aliases.zsh
+++
@@ -1,9 +0,0 @@
-alias g='git'
-alias ll='ls -lah'
-alias path='echo -e ${PATH//:/\\n}'
-alias c='clear'
-alias qr='pbpaste | qrencode -t ansiutf8'
-
-# override -h and --help to use bat
-alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
-alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
